4437ca82b31bd0b50a826565c7352607c40abc6e
jaewooklim1 <jaewooklim1@gmail.com>
2021-04-07 17:29:06 -0400

    finished application.html.erb

--- Goals_app/app/controllers/application_controller.rb
+++ Goals_app/app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :login!
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!@current_user 
+    end
+
+    def logged_out!
+        if logged_in?
+            @current_user.reset_session_token!
+        end
+
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
 end
